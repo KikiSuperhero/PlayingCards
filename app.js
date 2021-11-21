6627be77f2e9f2943db240f50c73aaccb54e4174
KikiSuperhero <held_chr@web.de>
2021-11-21 16:05:29 +0100

    fixed the 'new deck' api call

--- app.js
+++ app.js
@@ -3,12 +3,21 @@ const startButton = document.querySelector("#start-button");
 var sDeckId = "";
 
 const getDeckId = async () => {
+  //debugger;
   try {
-    let cardDeck = axios.get("https://deckofcardsapi.com/api/deck/new/");
-    console.log(cardDeck);
-  } catch (e) {}
-  console.log("Could not card data!");
-  console.log("Error: " + e);
+    let oNewDeckPromise = await axios.get(
+      "https://deckofcardsapi.com/api/deck/new/"
+    );
+    let oNewDeckPromiseResponse = oNewDeckPromise.data;
+    console.log(
+      "We made a new deck with following data: ",
+      oNewDeckPromiseResponse
+    );
+    sDeckId = oNewDeckPromiseResponse.deck_id;
+  } catch (e) {
+    console.log("Could not card data!");
+    console.log("Error: " + e);
+  }
 };
 
 startButton.addEventListener("click", getDeckId);
